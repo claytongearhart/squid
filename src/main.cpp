24afb3a961a1995ee925938a375f25bf96f6e040
Clayon Gearhart <claytongearhart240@gmail.com>
2021-09-18 10:33:16 -0400

    did stuff

--- src/main.cpp
+++ src/main.cpp
@@ -8,45 +8,46 @@
 
 int main()
 {
-  scanner mainScanner;
-  squid::console appConsole;
+    scanner mainScanner;
+    squid::console appConsole;
 
-  std::ifstream source("test_.cpp");
-  std::string sourceString((std::istreambuf_iterator<char>(source)),
-                           std::istreambuf_iterator<char>());
+    std::ifstream source("test_.cpp");
+    std::string sourceString((std::istreambuf_iterator<char>(source)),
+                             std::istreambuf_iterator<char>());
 
-  mainScanner.analyze(sourceString);
-  auto tokenList = mainScanner.fullTokens;
-  for (int i = 0; i < tokenList.size(); i++)
-  {
-    std::cout << tokenList[i].value << " : " << tokenList[i].type << '\n';
-  }
-
-  unsigned int mainAt;
-  bool mainExist;
-
-  std::vector<squid::bracket> bracketLocations;
-
-  for (int i = 0; i < tokenList.size(); i++)
-  {
-    if (tokenList[i].value == "int" &&
-        tokenList[i].type == squid::keywordToken &&
-        tokenList[i + 1].value == "main")
+    mainScanner.analyze(sourceString);
+    auto tokenList = mainScanner.fullTokens;
+    for (int i = 0; i < tokenList.size(); i++)
     {
-      mainAt = i + 1;
-      bool mainExist = true;
+        std::cout << tokenList[i].value << " : " << tokenList[i].type
+                  << '\n';
     }
-    // Search for braces
-    if (tokenList[i].value == "{" &&
-        tokenList[i].type == squid::delimiterToken)
+
+    unsigned int mainAt;
+    bool mainExist;
+
+    std::vector<squid::bracket> bracketLocations;
+
+    for (int i = 0; i < tokenList.size(); i++)
     {
-      bracketLocations.emplace_back(squid::bracket(i, true));
+        if (tokenList[i].value == "int" &&
+            tokenList[i].type == squid::keywordToken &&
+            tokenList[i + 1].value == "main")
+        {
+            mainAt = i + 1;
+            bool mainExist = true;
+        }
+        // Search for braces
+        if (tokenList[i].value == "{" &&
+            tokenList[i].type == squid::delimiterToken)
+        {
+            bracketLocations.emplace_back(squid::bracket(i, true));
+        }
+        if (tokenList[i].value == "}" &&
+            tokenList[i].type == squid::delimiterToken)
+        {
+            bracketLocations.emplace_back(squid::bracket(i, false));
+        }
     }
-    if (tokenList[i].value == "}" &&
-        tokenList[i].type == squid::delimiterToken)
-    {
-      bracketLocations.emplace_back(squid::bracket(i, false));
-    }
-  }
 }
 // tokenList[bracketLocations[i].position]
